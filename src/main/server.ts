c53223bacf32fab1ff0aba62d3cbd9993ce91d13
Lucas Santos <lucasa95santos@gmail.com>
2022-02-24 18:14:15 -0400

    feat: configurando o typeorm e o sqlite3 no projeto

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,8 @@
 import { App } from './config/app';
 import { Routes } from './config/routes';
 import env from './config/env';
+// database
+import '../database/connection';
 
 const httpServer = App.getInstance(Routes);
 
